ccfcdf48bb90ab8a6e9660a438f846c7b0406f17
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-12-20 14:54:17 +0530

    documentation

--- LAB 2/Functions/prog.c
+++ LAB 2/Functions/prog.c
@@ -1,18 +1,18 @@
 /*
 *Name - Nikhil Ranjan Nayak
 *Regd No - 1641012040
-*Desc -	PID
+*Desc -	Functions
 */
 #include "stdio.h"
 #include "unistd.h"
 
 void main()
 {
-	printf("PID: %ld\n", (long)getpid());
-	printf("PPID: %ld\n", (long)getppid());
-	printf("UID: %ld\n", (long)getuid());
-	printf("GID: %ld\n", (long)getgid());
-	printf("EGID: %ld\n", (long)getegid());
-	printf("EUID: %ld\n", (long)geteuid());
+	printf("PID: %ld\n", (long)getpid());	// returns process id
+	printf("PPID: %ld\n", (long)getppid());	// returns parent process id
+	printf("UID: %ld\n", (long)getuid());	// returns user id
+	printf("GID: %ld\n", (long)getgid());	// returns group id
+	printf("EGID: %ld\n", (long)getegid());	// returns effective group id
+	printf("EUID: %ld\n", (long)geteuid()); // returns effective user id
 	while(1){}
 }
