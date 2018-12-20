cdbe5cea2ff49f5502c83bae1ac0c62fb4aa2150
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-12-20 14:49:00 +0530

    more functions

--- LAB 2/Functions/prog.c
+++ LAB 2/Functions/prog.c
@@ -10,5 +10,9 @@ void main()
 {
 	printf("PID: %ld\n", (long)getpid());
 	printf("PPID: %ld\n", (long)getppid());
+	printf("UID: %ld\n", (long)getuid());
+	printf("GID: %ld\n", (long)getgid());
+	printf("EGID: %ld\n", (long)getegid());
+	printf("EUID: %ld\n", (long)geteuid());
 	while(1){}
 }
