f4c248a3e9ca92d2142d25aa69fdb8f6176004ff
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-12-21 11:25:35 +0530

    cla implementation

--- LAB 3/prog1.c
+++ LAB 3/prog1.c
@@ -5,9 +5,37 @@
 */
 #include "stdio.h"
 #include "unistd.h"
+void fork_thrice();
+void fork_quad();
 
-void main()
+void main(int argc, char *argv[])
 {
+	if(argc >= 2)
+	{
+		if(*argv[1] == '3')
+			fork_thrice();
+		else if(*argv[1] == '4')
+			fork_quad();
+		else
+			printf("%c\n", *argv[1]);
+	}
+	else
+		printf("Not Enough Arugments\n");
+}
+
+void fork_thrice()
+{
+	printf("\n****Thrice forked****\n");
+	fork();
+	fork();
+	fork();
+	printf("Hello, PID: %ld \n", (long)getpid());
+}
+
+void fork_quad()
+{
+	printf("\n****Quad forked****\n");
+	fork();
 	fork();
 	fork();
 	fork();
